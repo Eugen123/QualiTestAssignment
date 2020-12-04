31131dac5a2045ab3c7c729620c0c2df42130e28
eugen.dragomir <dragomir.eugen@yahoo.com>
2020-12-04 17:02:17 +0200

    add list test

--- qualyassignment/src/main/java/ListManipulator.java
+++ qualyassignment/src/main/java/ListManipulator.java
@@ -1,6 +1,36 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ListManipulator {
+
+
+    public Map<String, String> filterFileTypes(List<String> input){
+
+        Map<String, String> result = new HashMap<String, String>();
+        String audio = "";
+        String video = "";
+        String other = "";
+
+        for (String s:
+             input) {
+            if (s.contains(".mp4")) {
+                video += s + " ";
+            } else if (s.contains(".wav")) {
+                audio += s + " ";
+
+            } else {
+                other += s + " ";
+            }
+        }
+
+        result.put("Video", video);
+        result.put("Audio", audio);
+        result.put("Other", other);
+
+        return result;
+
+    }
 
 
 }
