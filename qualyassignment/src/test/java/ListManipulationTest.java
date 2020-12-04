15d3e106425e556fb8b6bbe1a2b26fa106b3cc7a
eugen.dragomir <dragomir.eugen@yahoo.com>
2020-12-04 17:21:23 +0200

    add test ng dependency

---
+++ qualyassignment/src/test/java/ListManipulationTest.java
@@ -0,0 +1,35 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ListManipulationTest {
+
+    @Test
+    public void listTest(){
+        ListManipulator listManipulator = new ListManipulator();
+
+        List<String> inputList = new ArrayList<String>();
+
+        inputList.add("Entry One.mp4");
+        inputList.add("Entry Two.wav");
+        inputList.add("Entry Three.jpg");
+        inputList.add("Entry Four.mng");
+        inputList.add("Entry Five.png");
+        inputList.add("Entry Six.csv");
+
+        Map<String, String> result = listManipulator.filterFileTypes(inputList);
+
+        System.out.print(result);
+
+        Assert.assertEquals(result.get("Video").trim(), "Entry One.mp4");
+        Assert.assertEquals(result.get("Audio").trim(), "Entry Two.wav");
+        Assert.assertTrue(result.get("Other").trim().contains("Entry Three.jpg"));
+        Assert.assertTrue(result.get("Other").trim().contains("Entry Four.mng"));
+        Assert.assertTrue(result.get("Other").trim().contains("Entry Five.png"));
+        Assert.assertTrue(result.get("Other").trim().contains("Entry Six.csv"));
+
+    }
+}
--- qualyassignment/src/test/java/StringManipulationTest.java
+++ qualyassignment/src/test/java/StringManipulationTest.java
@@ -1,11 +1,6 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 
 public class StringManipulationTest {
 
@@ -21,31 +16,5 @@ public class StringManipulationTest {
         StringManipulator stringManipulator = new StringManipulator();
 
         stringManipulator.printPhraseInPhrame("Hello word in a frame");
-    }
-
-    @Test
-    public void listTest(){
-        ListManipulator listManipulator = new ListManipulator();
-
-        List<String> inputList = new ArrayList<String>();
-
-        inputList.add("Entry One.mp4");
-        inputList.add("Entry Two.wav");
-        inputList.add("Entry Three.jpg");
-        inputList.add("Entry Four.mng");
-        inputList.add("Entry Five.png");
-        inputList.add("Entry Six.csv");
-
-        Map<String, String> result = listManipulator.filterFileTypes(inputList);
-
-        System.out.print(result);
-
-        Assert.assertEquals(result.get("Video").trim(), "Entry One.mp4");
-        Assert.assertEquals(result.get("Audio").trim(), "Entry Two.wav");
-        Assert.assertTrue(result.get("Other").trim().contains("Entry Three.jpg"));
-        Assert.assertTrue(result.get("Other").trim().contains("Entry Four.mng"));
-        Assert.assertTrue(result.get("Other").trim().contains("Entry Five.png"));
-        Assert.assertTrue(result.get("Other").trim().contains("Entry Six.csv"));
-
     }
 }
